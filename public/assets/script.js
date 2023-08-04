7ae284f68ff2f1105a017867bf1c3b7eea33bbc8
Espar-RodBar <cr4wl3r@gmail.com>
2023-08-04 21:48:37 +0200

    added event edit buton

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,13 +1,27 @@
 console.log("page loaded");
 setInterval(() => location.reload(), 50000);
 
+const editBtnEls = document.querySelectorAll(".message_edit");
 const deleteTextEls = document.querySelectorAll(".message_delete");
 const likeEls = document.querySelectorAll(".message_likes");
+
+Array.from(editBtnEls).forEach((btn) => {
+    btn.addEventListener("click", activateEditMessage);
+});
 
 Array.from(deleteTextEls).forEach((el) =>
     el.addEventListener("click", deleteMsg)
 );
 Array.from(likeEls).forEach((el) => el.addEventListener("click", addLike));
+
+async function activateEditMessage() {
+    const overlayContainer = document.querySelector(".overlay");
+    console.log(overlayContainer);
+    overlayContainer.classList.remove("no_visible", "no_display");
+}
+
+//TODO: Close modal window
+// end
 
 async function addLike() {
     const messageId = this.parentNode.dataset.messageId;
