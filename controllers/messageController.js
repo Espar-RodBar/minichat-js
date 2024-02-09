2d47d484eb6c5100076ba47adac108ff66b88ee3
Espar-RodBar <cr4wl3r@gmail.com>
2024-02-09 20:42:34 +0100

    Now restore all the messages from the database

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,20 +3,12 @@ const saveMessage = require('../helpers/saveMessage')
 
 exports.getMessages = async (req, res) => {
   try {
+    const messages = await messageModel.find()
+    console.log(messages)
+    res.render('chatRoom.ejs', { messages })
   } catch (error) {
     console.log(error)
   }
-  const messages = []
-  const message = {}
-  message.id = 1
-  message.text = 'hola q hase'
-  message.userName = 'yo'
-  message.likes = 0
-  // messageModel
-  //   .find()
-  //   .toArray()
-  //   .then((messages) => {})
-  res.render('chatRoom.ejs', { messages })
 }
 
 exports.sendMessage = (req, res) => {
